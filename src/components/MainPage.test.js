fd4b4beaeb34bd219d8603890729522428e29dfd
TraceyTaylor <107629102+TraceyTaylor@users.noreply.github.com>
2022-07-05 08:27:07 -0500

    test

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -1,22 +1,38 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import MainPage from './MainPage';
+import { shallow } from "enzyme";
+import React from "react";
+import MainPage from "./MainPage";
 
 let wrapper;
+
 beforeEach(() => {
-    const mockProps = {
-        onRequestRobots: jest.fn(),
-        robots: [],
-        searchfield: '',
-        isPending: false
-    }
-    wrapper = shallow(<MainPage{...mockProps}/>)
-})
+  const mockProps = {
+    onRequestRobots: jest.fn(),
+    robots: [],
+    searchField: "",
+    isPending: false,
+  };
+  wrapper = shallow(<MainPage {...mockProps} />);
+});
 
-it('renders MainPage without crashing', () => {
-    expect(wrapper).toMatchSnapshot();
-})
+it("renders MainPage without crashing", () => {
+  expect(wrapper).toMatchSnapshot();
+});
 
-it('filters robots correctly', () => {
-    expect(wrapper.instance())
-})
+it("filters robots correctly", () => {
+  const mockProps2 = {
+    onRequestRobots: jest.fn(),
+    robots: [{
+        id: 3,
+        name: "John",
+        email: "john@gmail.com",
+      }],
+    searchfield: "john", // was "a"
+    isPending: false
+  };
+  const wrapper2 = shallow(<MainPage {...mockProps2} />);
+  expect(wrapper2.instance().filterRobots()).toEqual([{
+      id: 3,
+      name: "John",
+      email: "john@gmail.com",
+    }]);
+});
